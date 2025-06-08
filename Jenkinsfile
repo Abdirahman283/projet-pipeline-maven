ecfac43e05df0a3a7c1d777923e064a0c4763910
eleve <eleve@Big-data.myguest.virtualbox.org>
2025-06-08 18:27:37 -0400

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+pipeline {
+  agent any
+
+  stages {
+    stage('Exemple') {
+      steps {
+        echo 'Bonjour depuis Jenkins'
+      }
+    }
+  }
+}
---
+++ src/main/java/com/exemple/App.java
@@ -0,0 +1,11 @@
+package com.exemple;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins + Maven");
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+}
